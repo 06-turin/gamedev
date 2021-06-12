66e82060cf1190bff475176c8569ece817fa12a6
Marat <m.mukhutdinov@agora.ru>
2021-06-12 17:01:04 +0800

    Задана структура проекта, добавлены webpack, postcss, eslint

---
+++ src/components/organisms/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+import './App.css';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Здесь будет игра</h1>
+      </div>
+    );
+  }
+}
+export default App;
