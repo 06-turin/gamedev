1817bbbc652de6668ad55616bbac2d735c25d0ab
Tim <jasper7466@yandex.ru>
2021-06-15 19:41:42 +0500

    Вёрстка. Страница регистрации

--- src/components/molecules/Form/Form.tsx
+++ src/components/molecules/Form/Form.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
 import { GDTextInput, GDTextInputProps } from 'components/atoms/GDTextInput/GDTextInput';
+import classnames from 'classnames';
 import './styles.css';
 
 type FormProps = {
-  fields: GDTextInputProps[];
+  fields: GDTextInputProps[]
+  className?: string
 }
 
-export const Form: FC<FormProps> = ({ fields }) => (
-  <form className="form">
+export const Form: FC<FormProps> = ({ fields, className }) => (
+  <form className={classnames(['form', className])}>
     {fields.map((field) => <GDTextInput id={field.id} title={field.title} />)}
+    <label className="form__error-label">validation error !</label>
   </form>
 );
--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,7 +1,27 @@
 import React, { FC } from 'react';
+import { GDButton } from 'components/atoms/GDButton/GDButton';
+import { Form } from 'components/molecules/Form';
+import './styles.css';
 
-export const Registration: FC = () => (
-  <div>
-    <h1>Registration</h1>
-  </div>
+const loginFormFields = [
+  { id: 'first_name', title: 'name' },
+  { id: 'second_name', title: 'last name' },
+  { id: 'email', title: 'e-mail', type: 'email' },
+  { id: 'phone', title: 'phone', type: 'tel' },
+  { id: 'login', title: 'login' },
+  { id: 'password', title: 'password', type: 'password' },
+  { id: 'verify_password', title: 'repeat', type: 'password' },
+];
+
+export const RegistrationComponent: FC = () => (
+  <section className="page">
+    <header className="page__header">
+      <h1 className="page__title">registration</h1>
+    </header>
+    <Form className="register-form" fields={loginFormFields} />
+    <GDButton className="page__footer-item" title="submit" styleOption="primary" size="l" onClick={() => null} />
+    <footer className="page__footer">
+      <GDButton className="page__footer-item" title="back" styleOption="secondary" size="l" onClick={() => null} />
+    </footer>
+  </section>
 );
