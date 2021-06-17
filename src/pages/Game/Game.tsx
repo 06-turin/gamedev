07ea3048a1a6fd672fd274300c6e4d57f765ccf1
Viktor Prilepin <vicpride@gmail.com>
2021-06-17 21:00:05 +0700

    Lints fixing

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Game as GameComponent } from 'components/organisms/Game/Game';
 
-export const GamePlay: FC = () => (
+export const Game: FC = () => (
   <GameComponent />
 );
