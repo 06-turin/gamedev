32a18dd64acc509e9c0e24cab48ec29e7f0ca35e
Viktor Prilepin <vicpride@gmail.com>
2021-07-24 18:11:12 +0700

    clear index.js

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// const path = require('path');
-// const express = require('express');
-
-// const app = express();
-
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist/index.html'), (err) => {
-//     if (err) {
-//       res.status(500).send(err);
-//     }
-//   });
-// });
-
 const { app } = require('./dist/server.js');
 
 app.set('port', process.env.PORT || 8080);
