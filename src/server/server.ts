46288fa3f88e4045920fc6f6f94779bf35071ebf
Viktor Prilepin <vicpride@gmail.com>
2021-07-29 17:30:26 +0700

    Перенес HMR посредника в .use

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,18 +1,15 @@
 import path from 'path';
 import express from 'express';
 import config from '../../webpackConfigs/client.config';
-// import { storeMiddleware } from './middlewares/storeMiddleware';
-// import { testDispatchMiddleware } from './middlewares/testDispatchMiddleware';
 import { IndexController } from './controllers/IndexController';
 import { webpackMiddlewares } from './middlewares/webpackMiddleware';
 
 const app = express();
 
 app
-  // .use(storeMiddleware)
-  // .use(testDispatchMiddleware)
+  .use(webpackMiddlewares(config))
   .use(express.static(path.join(__dirname, '../dist')));
 
-app.get(['/*'], webpackMiddlewares(config), IndexController.index);
+app.get(['/*'], IndexController.index);
 
 export { app };
