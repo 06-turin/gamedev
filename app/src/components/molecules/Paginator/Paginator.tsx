e57b0eb9ecd3a591f0e59dc7a1496896053a6ac8
Tim <jasper7466@yandex.ru>
2021-08-17 16:39:35 +0500

    Paginator. Добавлен компонент для пагинации

---
+++ app/src/components/molecules/Paginator/Paginator.tsx
@@ -0,0 +1,46 @@
+import './styles.css';
+import React, { FC, MouseEventHandler, useState } from 'react';
+import classNames from 'classnames';
+
+export type TPaginatorProps = {
+  pagesCount: number
+  currentPage: number
+  pageChanger: (pageNumber: number) => void
+}
+
+const range = (start = 1, count = 10) => Array(count).fill(0).map((_, index) => start + index);
+
+export const Paginator: FC<TPaginatorProps> = ({ pagesCount, currentPage, pageChanger }) => {
+  const items = range(1, pagesCount);
+  const [page, setPage] = useState(currentPage);
+
+  const clickHandler: MouseEventHandler = (event) => {
+    const target = event.target as HTMLElement;
+    if (target.textContent) {
+      const pageNumber = parseInt(target.textContent, 10);
+      pageChanger(pageNumber);
+      setPage(pageNumber);
+    }
+  };
+
+  return (
+    <nav className={classNames(['paginator'])}>
+      <div className={classNames(['paginator__list'])}>
+        {items.map((number, index) => (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+          <span
+            role="button"
+            tabIndex={index}
+            className={classNames([
+              'paginator__item',
+              number === page && 'paginator__item_active',
+            ])}
+            onClick={clickHandler}
+          >
+            {number}
+          </span>
+        ))}
+      </div>
+    </nav>
+  );
+};
